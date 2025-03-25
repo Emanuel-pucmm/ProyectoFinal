68db0b6d45f30f4538d0cbdeb47de532acb6d653
Owner <Owner@DESKTOP-H5BGHOD>
2025-03-24 23:03:51 -0400

    creando la clase Pitcher y bateador

---
+++ ProyectoFinalBeisbol/src/logico/Bateador.java
@@ -0,0 +1,38 @@
+package logico;
+
+public class Bateador extends Jugador {
+    private String posicion;
+    private BateadorEstadisticas statsBateador;
+
+    public Bateador(String nombre, LocalDate fechaNacimiento, String edad, 
+                   String nacionalidad, String manoDominante, LocalDate fechaDebut, 
+                   double altura, String posicion) {
+        super(nombre, fechaNacimiento, edad, nacionalidad, manoDominante, fechaDebut, altura);
+        this.posicion = posicion;
+        this.statsBateador = new BateadorEstadisticas();
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public BateadorEstadisticas getStatsBateador() {
+        return statsBateador;
+    }
+
+    public String obtenerPosicion() {
+        return this.posicion;
+    }
+
+    public BateadorEstadisticas obtenerEstadisticas() {
+        return this.statsBateador;
+    }
+
+    public void actualizarEstadisticas(BateadorEstadisticas nuevasStats) {
+        this.statsBateador = nuevasStats;
+    }
+}
---
+++ ProyectoFinalBeisbol/src/logico/Pitcher.java
@@ -0,0 +1,54 @@
+package logico;
+
+public class Pitcher extends Jugador {
+    private String tipoLanzador;
+    private ArrayList<String> tiposLanzamientos;
+    private PitcherEstadisticas statsPitcher;
+
+    public Pitcher(String nombre, LocalDate fechaNacimiento, String edad, 
+                 String nacionalidad, String manoDominante, LocalDate fechaDebut, 
+                 double altura, String tipoLanzador) {
+        super(nombre, fechaNacimiento, edad, nacionalidad, manoDominante, fechaDebut, altura);
+        this.tipoLanzador = tipoLanzador;
+        this.tiposLanzamientos = new ArrayList<>();
+        this.statsPitcher = new PitcherEstadisticas();
+    }
+
+    public String getTipoLanzador() {
+        return tipoLanzador;
+    }
+
+    public void setTipoLanzador(String tipoLanzador) {
+        this.tipoLanzador = tipoLanzador;
+    }
+
+    public ArrayList<String> getTiposLanzamientos() {
+        return new ArrayList<>(tiposLanzamientos);
+    }
+
+    public PitcherEstadisticas getStatsPitcher() {
+        return statsPitcher;
+    }
+
+    public String obtenerTipoPitcher() {
+        return this.tipoLanzador;
+    }
+
+    public PitcherEstadisticas obtenerEstadisticas() {
+        return this.statsPitcher;
+    }
+
+    public void actualizarEstadisticas(PitcherEstadisticas nuevasStats) {
+        this.statsPitcher = nuevasStats;
+    }
+
+    public void agregarLanzamiento(String tipoLanzamiento) {
+        if (tipoLanzamiento != null && !tipoLanzamiento.isEmpty()) {
+            this.tiposLanzamientos.add(tipoLanzamiento);
+        }
+    }
+
+    public ArrayList<String> obtenerLanzamientos() {
+        return new ArrayList<>(this.tiposLanzamientos);
+    }
+}
